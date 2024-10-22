e65694a731e91ea25265f6dd2467651d0a3c7607
YulliaAC <yul@appsclicks.com>
2024-10-22 23:29:54 +0300

    init

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const lodash_1 = __importDefault(require("lodash"));
+console.log(lodash_1.default.shuffle([1, 2, 3]));
+//# sourceMappingURL=app.js.map
