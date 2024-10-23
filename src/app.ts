9554d3ccfb35549cc0bfee9e52a1499c6d03a72f
yuliasemenyuk <semenyukyp@gmail.com>
2024-10-23 22:34:37 +0300

    basic markup

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,38 @@
-import { Product } from "./product.model";
-// import 'reflect-metadata';
-import { plainToClass } from "class-transformer";
-import { validate } from "class-validator";
 
-const products = [
-  { title: "A Carpet", price: 34.99 },
-  { title: "A Book", price: 23.99 },
-];
 
-// const loadedProducts = products.map(prod => {
-//     return new Product(prod.title, prod.price);
-// });
 
-const loadedProducts = plainToClass(Product, products)
 
-for (const prod of loadedProducts) {
-    console.log(prod.getInformation())
-}
 
-const newProd = new Product('', -6)
-validate(newProd).then(errors => {
-    if (errors.length > 0) {
-        console.log("Validation errors")
-        console.log(errors)
-    } else {
-        console.log(newProd.getInformation())
-    }
-})
+
+
+// import { Product } from "./product.model";
+// // import 'reflect-metadata';
+// import { plainToClass } from "class-transformer";
+// import { validate } from "class-validator";
+
+// const products = [
+//   { title: "A Carpet", price: 34.99 },
+//   { title: "A Book", price: 23.99 },
+// ];
+
+// // const loadedProducts = products.map(prod => {
+// //     return new Product(prod.title, prod.price);
+// // });
+
+// const loadedProducts = plainToClass(Product, products)
+
+// for (const prod of loadedProducts) {
+//     console.log(prod.getInformation())
+// }
+
+// const newProd = new Product('', -6)
+// validate(newProd).then(errors => {
+//     if (errors.length > 0) {
+//         console.log("Validation errors")
+//         console.log(errors)
+//     } else {
+//         console.log(newProd.getInformation())
+//     }
+// })
 
 
